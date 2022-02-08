a568e4eb05412ed7fc86178be466a86e941f6507
nandgini <nand@ginilytics.com>
2022-02-08 19:19:23 +0530

    task-6: database for task-6

--- database/ci_test.sql
+++ database/ci_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2022 at 12:49 PM
+-- Generation Time: Feb 08, 2022 at 02:48 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -42,7 +42,8 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `title`, `description`, `image`, `status`, `created_at`) VALUES
 (1, 'Mobile', 'Mobile', 'mobile.jpg', 1, '2022-02-08 09:31:53'),
-(2, 'Keyboard', 'Keyboard', 'mobile.jpg', 1, '2022-02-08 09:31:53');
+(2, 'Keyboard', 'Keyboard', 'mobile.jpg', 2, '2022-02-08 13:28:18'),
+(3, 'DSLR Camera', 'DSLR Camera', 'mobile.jpg', 1, '2022-02-08 12:55:56');
 
 -- --------------------------------------------------------
 
@@ -57,21 +58,22 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `role` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `is_email_verified` tinyint(1) NOT NULL
+  `is_email_verified` tinyint(1) NOT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'inactive'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`, `is_email_verified`) VALUES
-(1, 'Nand', 'nand1235@yopmail.com', '12345678', 1, '2022-02-08 08:41:06', 0),
-(2, 'Nand', 'nand@gmail.com', '12345678', 1, '2022-02-08 08:42:39', 0),
-(3, 'admin', 'admin@gmail.com', '1234', 1, '2022-02-08 08:47:55', 0),
-(4, 'test', 'nand@ginilytics.com', '123', 1, '2022-02-08 09:02:23', 0),
-(5, 'test', 'test@test.com', '111', 1, '2022-02-08 09:04:13', 0),
-(6, 'ttest', 'test@tge.com', '123', 1, '2022-02-08 09:04:41', 0),
-(7, '11', 'superadmin@gmail.com', '12', 1, '2022-02-08 09:11:20', 0);
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`, `is_email_verified`, `status`) VALUES
+(1, 'Nand', 'nand1235@yopmail.com', '12345678', 1, '2022-02-08 11:57:32', 1, 'active'),
+(2, 'Nand', 'nand@gmail.com', '12345678', 1, '2022-02-08 11:57:33', 1, 'active'),
+(3, 'admin', 'admin@gmail.com', '1234', 1, '2022-02-08 11:57:35', 1, 'active'),
+(4, 'test', 'nand@ginilytics.com', '123', 1, '2022-02-08 09:02:23', 0, 'inactive'),
+(5, 'test', 'test@test.com', '111', 1, '2022-02-08 09:04:13', 0, 'inactive'),
+(6, 'ttest', 'test@tge.com', '123', 1, '2022-02-08 11:57:37', 1, 'active'),
+(7, '11', 'superadmin@gmail.com', '12', 1, '2022-02-08 09:11:20', 0, 'inactive');
 
 -- --------------------------------------------------------
 
@@ -93,14 +95,14 @@ CREATE TABLE `user_products` (
 --
 
 INSERT INTO `user_products` (`id`, `product_id`, `user_id`, `quantity`, `price`, `created_at`) VALUES
-(1, 1, 1, 4, '100', '2022-02-08 11:30:49'),
-(2, 2, 1, 6, '200', '2022-02-08 11:31:36'),
-(3, 2, 1, 6, '300', '2022-02-08 11:33:19'),
-(4, 2, 1, 45, '5550', '2022-02-08 11:33:55'),
-(5, 1, 1, 7, '400', '2022-02-08 11:35:09'),
-(6, 2, 1, 6, '500', '2022-02-08 11:35:43'),
-(7, 1, 1, 45, '500', '2022-02-08 11:42:32'),
-(8, 1, 1, 5565, '3434', '2022-02-08 11:44:38');
+(1, 1, 1, 3, '100', '2022-02-08 13:30:39'),
+(2, 2, 1, 7, '200', '2022-02-08 13:30:38'),
+(3, 2, 1, 20, '300', '2022-02-08 13:30:35'),
+(4, 2, 1, 30, '5550', '2022-02-08 13:30:32'),
+(5, 1, 1, 5, '400', '2022-02-08 13:30:29'),
+(6, 2, 1, 5, '500', '2022-02-08 13:30:27'),
+(7, 1, 1, 40, '500', '2022-02-08 13:30:24'),
+(8, 1, 1, 10, '3434', '2022-02-08 13:30:22');
 
 --
 -- Indexes for dumped tables
@@ -132,7 +134,7 @@ ALTER TABLE `user_products`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -144,7 +146,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `user_products`
 --
 ALTER TABLE `user_products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
